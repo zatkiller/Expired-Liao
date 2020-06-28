f0184901b65e86b097614812b3631abb158d15ba
zatkiller <gheedylan@yahoo.com.sg>
2020-06-29 02:15:30 +0800

    sample code to create new entry in firebase db

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,128 +1,137 @@
-import { AsyncStorage } from "react-native";
+import { AsyncStorage } from 'react-native';
 import firebase from 'firebase';
 
-export const SIGNUP = "SIGNUP";
-export const LOGIN = "LOGIN";
-export const AUTHENTICATE = "AUTHENTICATE";
-export const LOGOUT = "LOGOUT";
+export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
+export const AUTHENTICATE = 'AUTHENTICATE';
+export const LOGOUT = 'LOGOUT';
 
 export const authenticate = (userId, token) => {
-	return (dispatch) => {
-		dispatch({ type: AUTHENTICATE, userId: userId, token: token });
-	};
+  return (dispatch) => {
+    dispatch({ type: AUTHENTICATE, userId: userId, token: token });
+  };
 };
 
 export const signup = (email, password) => {
-	return async (dispatch) => {
-		// const response = await fetch(
-		// 	"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCokHRdBoHmUb8bfqcRTk79GBrkpXj1r5k",
-		// 	{
-		// 		method: "POST",
-		// 		headers: {
-		// 			"Content-Type": "application/json",
-		// 		},
-		// 		body: JSON.stringify({
-		// 			email: email,
-		// 			password: password,
-		// 			returnSecureToken: true,
-		// 		}),
-		// 	}
-		// );
+  return async (dispatch) => {
+    // const response = await fetch(
+    // 	"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCokHRdBoHmUb8bfqcRTk79GBrkpXj1r5k",
+    // 	{
+    // 		method: "POST",
+    // 		headers: {
+    // 			"Content-Type": "application/json",
+    // 		},
+    // 		body: JSON.stringify({
+    // 			email: email,
+    // 			password: password,
+    // 			returnSecureToken: true,
+    // 		}),
+    // 	}
+    // );
 
-		// if (!response.ok) {
-		// 	const errorResData = await response.json();
-		// 	const errorId = errorResData.error.message;
-		// 	let message = "Something went wrong!";
-		// 	if (errorId === "EMAIL_EXISTS") {
-		// 		message = "This email exists already!";
-		// 	}
-		// 	throw new Error(message);
-		// }
+    // if (!response.ok) {
+    // 	const errorResData = await response.json();
+    // 	const errorId = errorResData.error.message;
+    // 	let message = "Something went wrong!";
+    // 	if (errorId === "EMAIL_EXISTS") {
+    // 		message = "This email exists already!";
+    // 	}
+    // 	throw new Error(message);
+    // }
 
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(function(user) {
-                console.log(user);
-            })
-            .catch(function(error) {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-            });
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function (user) {
+        console.log(user);
+      })
+      .catch(function (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('signup error:', errorCode, errorMessage);
+      });
 
-		// const resData = await response.json();
-		// console.log(resData);
-		// dispatch(authenticate(resData.localId, resData.idToken));
-		// saveDataToStorage(resData.idToken, resData.localId);
-	};
+    // const resData = await response.json();
+    // console.log(resData);
+    // dispatch(authenticate(resData.localId, resData.idToken));
+    // saveDataToStorage(resData.idToken, resData.localId);
+  };
 };
 
 export const login = (email, password) => {
-	return async (dispatch) => {
-		// const response = await fetch(
-		// 	"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCokHRdBoHmUb8bfqcRTk79GBrkpXj1r5k",
-		// 	{
-		// 		method: "POST",
-		// 		headers: {
-		// 			"Content-Type": "application/json",
-		// 		},
-		// 		body: JSON.stringify({
-		// 			email: email,
-		// 			password: password,
-		// 			returnSecureToken: true,
-		// 		}),
-		// 	}
-		// );
+  return async (dispatch) => {
+    // const response = await fetch(
+    // 	"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCokHRdBoHmUb8bfqcRTk79GBrkpXj1r5k",
+    // 	{
+    // 		method: "POST",
+    // 		headers: {
+    // 			"Content-Type": "application/json",
+    // 		},
+    // 		body: JSON.stringify({
+    // 			email: email,
+    // 			password: password,
+    // 			returnSecureToken: true,
+    // 		}),
+    // 	}
+    // );
 
-		// if (!response.ok) {
-		// 	const errorResData = await response.json();
-		// 	const errorId = errorResData.error.message;
-		// 	let message = "Something went wrong!";
-		// 	if (errorId === "EMAIL_NOT_FOUND") {
-		// 		message = "This email could not be found!";
-		// 	} else if (errorId === "INVALID_PASSWORD") {
-		// 		message = "This password is not valid!";
-		// 	}
-		// 	throw new Error(message);
-		// }
-        
+    // if (!response.ok) {
+    // 	const errorResData = await response.json();
+    // 	const errorId = errorResData.error.message;
+    // 	let message = "Something went wrong!";
+    // 	if (errorId === "EMAIL_NOT_FOUND") {
+    // 		message = "This email could not be found!";
+    // 	} else if (errorId === "INVALID_PASSWORD") {
+    // 		message = "This password is not valid!";
+    // 	}
+    // 	throw new Error(message);
+    // }
 
-        email = 'nig@ger.com';
-        password = '123123';
+    email = 'nig@ger.com';
+    password = '123123'; // to remove hardcoded values and fix form submit
+    // because form submit giving empty password
 
-        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-            .then(function() {
-                return firebase.auth().signInWithEmailAndPassword(email, password);
-            })
-            .then(function(user) {
-                console.log(user);
-            })
-            .catch(function(err){
-                console.log('login error:', err.code, err.message);
-            });
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(function () {
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+      })
+      .then(function (user) {
+        console.log(user);
+      })
+      .catch(function (err) {
+        console.log('login error:', err.code, err.message);
+      });
 
-		// const resData = await response.json();
+    // const resData = await response.json();
 
-		// dispatch(authenticate(resData.localId, resData.idToken));
-		// dispatch(authenticate(resData.localId, resData.idToken));
-		// saveDataToStorage(resData.idToken, resData.localId);
-	};
+    // dispatch(authenticate(resData.localId, resData.idToken));
+    // dispatch(authenticate(resData.localId, resData.idToken));
+    // saveDataToStorage(resData.idToken, resData.localId);
+  };
 };
 
 export const logout = () => {
-	// AsyncStorage.removeItem("userData");
-    firebase.auth().signOut().then(function() {
-        console.log('logout success');
-    }).catch(function(err){
-        console.log('logout error:', err);
+  // AsyncStorage.removeItem("userData");
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      console.log('logout success');
+    })
+    .catch(function (err) {
+      console.log('logout error:', err);
     });
-	return { type: LOGOUT };
+  return { type: LOGOUT };
 };
 
 const saveDataToStorage = (token, userId) => {
-	AsyncStorage.setItem(
-		"userData",
-		JSON.stringify({
-			token: token,
-			userId: userId,
-		})
-	);
+  AsyncStorage.setItem(
+    'userData',
+    JSON.stringify({
+      token: token,
+      userId: userId,
+    }),
+  );
 };
--- store/actions/food.js
+++ store/actions/food.js
@@ -1,127 +1,133 @@
-import Food from "../../models/Food";
+import Food from '../../models/Food';
 
-export const DELETE_FOOD = "DELETE_FOOD";
-export const CREATE_FOOD = "CREATE_FOOD";
-export const UPDATE_FOOD = "UPDATE_FOOD";
-export const SET_FOOD = "SET_FOOD";
-import * as firebase from "firebase";
+export const DELETE_FOOD = 'DELETE_FOOD';
+export const CREATE_FOOD = 'CREATE_FOOD';
+export const UPDATE_FOOD = 'UPDATE_FOOD';
+export const SET_FOOD = 'SET_FOOD';
+import * as firebase from 'firebase';
 
 function writeUserData(ownerId, title, date, imageUrl, quantity) {
-	firebase.database().ref('users/' + userId).set({
-	  ownerId: userId,
-	  title: title,
-	  date: date,
-	  imageUrl : imageUrl,
-	  quantity: quantity
-	});
-  }
+  firebase
+    .database()
+    .ref('users/' + userId)
+    .set({
+      ownerId: userId,
+      title: title,
+      date: date,
+      imageUrl: imageUrl,
+      quantity: quantity,
+    });
+}
 
 export const fetchFood = () => {
-	return async (dispatch, getState) => {
-		const userId = getState().auth.userId;
-		try {
-			const response = await fetch(
-				"https://expired-liao.firebaseio.com/food.json"
-			);
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
+    try {
+      const response = await fetch(
+        'https://expired-liao.firebaseio.com/food.json',
+      );
 
-			if (!response.ok) {
-				throw new Error("Something went wrong!");
-			}
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-			const resData = await response.json();
-			const loadedFood = [];
+      const resData = await response.json();
+      const loadedFood = [];
 
-			for (const key in resData) {
-				loadedFood.push(
-					new Food(
-						key,
-						resData[key].ownerId,
-						resData[key].title,
-						resData[key].imageUrl,
-						resData[key].date,
-						resData[key].quantity
-					)
-				);
-			}
+      for (const key in resData) {
+        loadedFood.push(
+          new Food(
+            key,
+            resData[key].ownerId,
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].date,
+            resData[key].quantity,
+          ),
+        );
+      }
 
-			dispatch({
-				type: SET_FOOD,
-				food: loadedFood,
-				userFood: loadedFood.filter((food) => food.ownerId === userId),
-			});
-		} catch (err) {
-			throw err;
-		}
-	};
+      dispatch({
+        type: SET_FOOD,
+        food: loadedFood,
+        userFood: loadedFood.filter((food) => food.ownerId === userId),
+      });
+    } catch (err) {
+      throw err;
+    }
+  };
 };
 
 export const deleteFood = (foodId) => {
-	return async (dispatch, getState) => {
-		const token = getState().auth.token;
-		const response = await fetch(
-			`https://expired-liao.firebaseio.com/food/${foodId}.json?auth=${token}`,
-			{
-				method: "DELETE",
-			}
-		);
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(
+      `https://expired-liao.firebaseio.com/food/${foodId}.json?auth=${token}`,
+      {
+        method: 'DELETE',
+      },
+    );
 
-		if (!response.ok) {
-			throw new Error("Something went wrong!");
-		}
-		dispatch({ type: DELETE_FOOD, pid: foodId });
-	};
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    dispatch({ type: DELETE_FOOD, pid: foodId });
+  };
 };
 
 export const createFood = (title, date, imageUrl, quantity) => {
-	return async (dispatch) => {
-		const user = firebase.auth().currentUser;
-        try {
-            const response = await firebase.database().ref('food').push()
-                .set({
-                    title,
-                    date,
-                    imageUrl,
-                    quantity,
-                    ownerId: user.uid,
-                })
-            console.log(response);
-        } catch (err) {
-            console.log(err);
-        }
-	};
+  return async (dispatch) => {
+    const user = firebase.auth().currentUser;
+    try {
+      // pushes a new object under 'food' in the DB and sets that object's
+      // attributes
+      const response = await firebase.database().ref('food').push().set({
+        title,
+        date,
+        imageUrl,
+        quantity,
+        ownerId: user.uid,
+      });
+      console.log(response);
+      // probably wanna dispatch something here to add new food item to
+      // existing list of food items
+    } catch (err) {
+      console.log(err);
+    }
+  };
 };
 
 export const updateFood = (id, title, date, imageUrl, quantity) => {
-	return async (dispatch, getState) => {
-		const token = getState().auth.token;
-		const response = await fetch(
-			`https://expired-liao.firebaseio.com/food/${id}.json?auth=${token}`,
-			{
-				method: "PATCH",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					title,
-					date,
-					imageUrl,
-					quantity,
-				}),
-			}
-		);
-		if (!response.ok) {
-			throw new Error("Something went wrong!");
-		}
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(
+      `https://expired-liao.firebaseio.com/food/${id}.json?auth=${token}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          date,
+          imageUrl,
+          quantity,
+        }),
+      },
+    );
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
-		dispatch({
-			type: UPDATE_FOOD,
-			pid: id,
-			foodData: {
-				title,
-				date,
-				imageUrl,
-				quantity,
-			},
-		});
-	};
+    dispatch({
+      type: UPDATE_FOOD,
+      pid: id,
+      foodData: {
+        title,
+        date,
+        imageUrl,
+        quantity,
+      },
+    });
+  };
 };
